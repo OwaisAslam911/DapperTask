9d7a7608b41cfc21cb9c66530e28894bb03eb0c6
Owais Aslam <127122701+OwaisAslam911@users.noreply.github.com>
2024-11-07 06:31:12 -0800

    added

--- DapperTask/Models/EmployeeViewModel.cs
+++ DapperTask/Models/EmployeeViewModel.cs
@@ -4,11 +4,14 @@
     {
         public int EmployeeId { get; set; }
         public string EmployeeName { get; set; }
-        public string PositionTitle { get; set; }
-        public string DepartmentName { get; set; }
-        public string OrganizationName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
         public decimal Salary { get; set; }
+        public int PositionId { get; set; }  // Make sure this exists
+        public int DepartmentId { get; set; }
+        public int OrganizationId { get; set; }
+        public string PositionTitle { get; set; }  // Additional info for displaying
+        public string DepartmentName { get; set; }
+        public string OrganizationName { get; set; }
     }
 }
---
+++ DapperTask/Models/FilterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using DapperTask.Models;
+
+namespace DapperTask.Models
+{
+    public class FilterViewModel
+    {
+        public int? SelectedOrganization { get; set; }
+        public int? SelectedDepartment { get; set; }
+        public int? SelectedPosition { get; set; }
+        public IEnumerable<Organizations> Organizations { get; set; }
+        public IEnumerable<Departments> Departments { get; set; }
+        public IEnumerable<Positions> Positions { get; set; }
+        public IEnumerable<EmployeeViewModel> Employees { get; set; }
+
+    }
+}
