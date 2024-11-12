562cf515d4bd435aec040a6956ce4c497dcca880
Owais Aslam <127122701+OwaisAslam911@users.noreply.github.com>
2024-11-12 19:21:11 +0500

    updated view models

--- DapperTask/Models/Organizations.cs
+++ DapperTask/Models/Organizations.cs
@@ -8,7 +8,7 @@
         public DateTime FoundedDate { get; set; }
         public DateTime UpdatedAt { get; set; }
         public string UpdatedBy { get; set; }
-        public string CompanyEmail { get; set; }
+       
         public string Phone { get; set; }
         public string Location { get; set; }
         public DateTime CreatedAt { get; set; }
--- DapperTask/Models/PositionMappingViewModel.cs
+++ DapperTask/Models/PositionMappingViewModel.cs
@@ -1,13 +1,20 @@
-﻿namespace DapperTask.Models
+﻿using DapperTask.Models;
+
+namespace DapperTask.Models
 {
     public class PositionMappingViewModel
     {
         public int PostMapId { get; set; }
         public int OrganizationId { get; set; }
-        public string OrganizationName { get; set; } // New property for organization name
+        public string OrganizationName { get; set; }
         public int DepartmentId { get; set; }
-        public string DepartmentName { get; set; } // Existing property for department name
+        public string DepartmentName { get; set; }
         public int PositionId { get; set; }
-        public string PositionTitle { get; set; } // Existing property for position title
+        public string PositionTitle { get; set; }
+
+        public List<Organizations> Organizations { get; set; }
+        public List<Departments> Departments { get; set; }
+        public List<Positions> Positions { get; set; }
+        public List<PositionMappingViewModel> PositionMappings { get; set; }
     }
 }
