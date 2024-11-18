92327c3d687a9851a3bda299986da2462eea8b4f
Owais Aslam <127122701+OwaisAslam911@users.noreply.github.com>
2024-11-18 20:30:04 +0500

    f

--- DapperTask/Models/PositionMappingViewModel.cs
+++ DapperTask/Models/PositionMappingViewModel.cs
@@ -6,15 +6,12 @@ namespace DapperTask.Models
     {
         public int PostMapId { get; set; }
         public int OrganizationId { get; set; }
-        public string OrganizationName { get; set; }
+        public string? OrganizationName { get; set; }
         public int DepartmentId { get; set; }
-        public string DepartmentName { get; set; }
+        public string? DepartmentName { get; set; }
         public int PositionId { get; set; }
-        public string PositionTitle { get; set; }
+        public string? PositionTitle { get; set; }
 
-        public List<Organizations> Organizations { get; set; }
-        public List<Departments> Departments { get; set; }
-        public List<Positions> Positions { get; set; }
-        public List<PositionMappingViewModel> PositionMappings { get; set; }
+
     }
 }
