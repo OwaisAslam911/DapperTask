ca43be4d2d2c3fde34d01e73c5ee5e2ed1a2f968
Owais Aslam <127122701+OwaisAslam911@users.noreply.github.com>
2024-11-22 20:43:40 +0500

    added login signup

---
+++ DapperTask/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace DapperTask.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? UserEmail { get; set; }
+        public string? UserPassword { get; set; }
+    }
+}
