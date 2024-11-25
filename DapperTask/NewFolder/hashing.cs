632f67734c50520c88a7874dc8351327609bfeec
Owais Aslam <127122701+OwaisAslam911@users.noreply.github.com>
2024-11-25 19:13:36 +0500

    added hashing

---
+++ DapperTask/NewFolder/hashing.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+
+namespace DapperTask.NewFolder
+{
+    public class hashing
+    {
+
+
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                // Send a sample text to hash.  
+                var hashedPassword = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                
+                // Print the string.   
+                Console.WriteLine(hashedPassword);
+                return Convert.ToBase64String(hashedPassword);
+            }
+        }
+
+    }
+}
